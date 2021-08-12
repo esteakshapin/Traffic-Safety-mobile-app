1941b987a7b3f968452bb2a618b219caf599b698
esteakshapin(pop_os) <esteakshapin@gmail.com>
2021-08-12 13:14:07 -0400

    build added

---
+++ build/app/generated/source/buildConfig/release/com/cdma/youth_traffic_safety_town_hall/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cdma.youth_traffic_safety_town_hall;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.cdma.youth_traffic_safety_town_hall";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
